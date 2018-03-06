bd13ed79a9b96bee176e8928118e7585d658e7e3
Ivon <ivon.sanchez@polimentes.com>
2018-03-06 16:37:20 -0600

    feat: player model creation closes #1

--- players/models.py
+++ players/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+
+class Player(models.Model):
+    GENDER_CHOICES = (
+        ('F', 'Femenino'),
+        ('M', 'Masculino'),
+        ('O', 'Otro'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    nickname = models.CharField(max_length=20, blank=True, default='')
+    phone_number = models.CharField(max_length=15, null=False)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    # Se queda pendiente
+    dob = models.DateField(null=True, default=None)
+    web_site = models.CharField(max_length=400, blank=True, default='')
+    account_holder = models.CharField(max_length=40, blank=True, default='')
+    account_email = models.EmailField(max_length=254, blank=True, default='')
+
+    coins = models.BigIntegerField()
+    lives = models.IntegerField(default=0, validators=[
+                MaxValueValidator(5),
+                MinValueValidator(0),
+                ])
+    rematchs = models.IntegerField(default=0, validators=[
+                MaxValueValidator(5),
+                MinValueValidator(0),
+                ])
