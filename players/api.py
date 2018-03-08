71de22a34f9108e027acb1f6880274022cff24e2
Ivon <ivon.sanchez@polimentes.com>
2018-03-08 16:17:44 -0600

    feat: get player information JSON type

--- players/api.py
+++ players/api.py
@@ -1,24 +1,57 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from rest_framework.response import Response
+
+from players.models import Player
+from players.serializers import PlayerSerializer
 # Test test
 # Another test
 # fix
 
+
 class PlayerViewSet(viewsets.GenericViewSet):
-    permission_classes = []
-    authentication_classes = []
+    # permission_classes = (IsAuthenticated,)
+    # authentication_classes = []
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    filter_fields = ("")
 
     def list(self, request):
-
-        pass
+        queryset = Player.objects.all()
+        serializer = PlayerSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk):
-        pass
-
-    def create(self, request):
-        pass
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
     def partial_update(self, request, pk):
         pass
 
     def delete(self, request, pk):
         pass
+
+
+# class PlayerViewSet(viewsets.GenericViewSet):
+#     permission_classes = []
+#     authentication_classes = []
+#
+#     def list(self, request):
+#
+#         pass
+#
+#     def retrieve(self, request, pk):
+#         pass
+#
+#     def create(self, request):
+#         pass
+#
+#     def partial_update(self, request, pk):
+#         pass
+#
+#     def delete(self, request, pk):
+#         pass
--- players/serializers.py
+++ players/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from mobile_api.player_dao import PlayerDao
+from players.models import Player
+
 
 
 class UserDataSerializer(serializers.ModelSerializer):
@@ -11,11 +12,28 @@ class UserDataSerializer(serializers.ModelSerializer):
 class UserGetSerializer(serializers.Serializer):
 
     def list(self):
-        result = PlayerDao.list(self.data)
-        return result
+        pass
 
     def retrieve(self):
         pass
 
     def delete(self):
         pass
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+    # def validate_player_id(self, value):
+    #     try:
+    #         Player.objects.request(pk=value)
+    #         return value
+    #     except:
+    #         serializer.ValidationError("El jugador no existe")
+
+    # def get_serializer_class(self):
+    #     if self.request.user.is_staff:
+    #         return FullAccountSerializer
+    #     return BasicAccountSerializer
